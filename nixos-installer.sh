1446c1320c0eb17ce9147a7a9f8ffda53c434680
Wynand Marais <info@wynandmarais.com>
2021-09-24 11:40:32 +0000

    Made minor bug fixes to get the first pass working.

--- nixos-installer.sh
+++ nixos-installer.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 HDD=/dev/sda
 
@@ -20,6 +20,8 @@ LUKS_VOL_NAME=crypt3
 LUKS_KEY="luks_key.txt"
 
 SWAP_SIZE=2G
+ROOT_SIZE=100%
+
 
 LVM_VG_POOL_NAME=sysvg
 LVM_SWAP_LV_NAME=swaplv
@@ -27,35 +29,37 @@ LVM_ROOT_LV_NAME=rootlv
 
 
 check_error() {
-  >&2 echo "ERROR - $1"
-  exit 1
+  if [ $? -ne 0 ]; then
+    >&2 echo "ERROR - $1"
+    exit 1
+  fi
 }
 
 # Create the partition table on the HDD.
 parted -s -a optimal ${HDD} mklabel gpt
-check_error("Failed to create partition table.")
+check_error "Failed to create partition table."
 
 # Create the EFI partition.
-parted -s -a optimal ${HDD} mkpart primary efi fat32 ${EFI_PART_START} ${EFI_PART_END}
-check_error("Failed to create EFI partition.")
+parted -s -a optimal ${HDD} mkpart ESP fat32 ${EFI_PART_START} ${EFI_PART_END}
+check_error "Failed to create EFI partition."
 
 parted -s ${HDD} set 1 esp on
-check_error("Failed to set the esp flag on the EFI partition.")
+check_error "Failed to set the esp flag on the EFI partition."
 
 # Create the Boot (/boot) partition.
-parted -s -a optimal ${HDD} mkpart primary boot ext4 ${BOOT_PART_START} ${BOOT_PART_END}
-check_error("Failed to create Boot (/boot) partition.")
+parted -s -a optimal ${HDD} mkpart primary ext4 ${BOOT_PART_START} ${BOOT_PART_END}
+check_error "Failed to create Boot (/boot) partition."
 
 # Create the encrypted partition.
-parted -s -a optimal ${HDD} mkpart primary ${LUKS_PART_NAME} ${LUKS_PART_START} ${LUKS_PART_END}
-check_error("Failed to create encrypted partition.")
+parted -s -a optimal ${HDD} mkpart primary ${LUKS_PART_START} ${LUKS_PART_END}
+check_error "Failed to create encrypted partition."
 
 # Mount the encrypted partition.
-cryptsetup luksFormat /dev/sda3 ${LUKS_KEY}
-check_error("Failed to format encrypted partition.")
+cryptsetup -q luksFormat /dev/sda3 ${LUKS_KEY}
+check_error "Failed to format encrypted partition."
 
-cryptsetup luksOpen /dev/sda3 ${LUKS_VOL_NAME} --key-file ${LUKS_KEY}
-check_error("Failed to open encrypted partition.")
+cryptsetup -q luksOpen /dev/sda3 ${LUKS_VOL_NAME} --key-file ${LUKS_KEY}
+check_error "Failed to open encrypted partition."
 
 # Create the LVM physical volume and volume group.
 pvcreate /dev/mapper/${LUKS_VOL_NAME} 
