dc568b184337a596f336773615635b0d2860f17f
Wynand Marais <info@wynandmarais.com>
2021-09-25 18:27:24 +0930

    Setup the boot partition.

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -15,6 +15,11 @@
     device = "/dev/disk/by-label/rootlv";
     fsType = "ext4";
   };
+  
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/boot";
+    fsType = "ext4";
+  };
 
   fileSystems."/boot/efi" = {
     device = "/dev/disk/by-label/EFI";
