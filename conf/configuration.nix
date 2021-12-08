8aac0375ce504f003ee5e5523d4f9f7a39344e0e
Wynand Marais <info@wynandmarais.com>
2021-12-08 18:22:26 +1030

    Added some code to try and wrap up users creation.

--- conf/configuration.nix
+++ conf/configuration.nix
@@ -11,6 +11,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./conf/fs.nix
+      ./conf/users.nix
     ];
 
   # networking.hostName = "nixos"; # Define your hostname.
--- install.sh
+++ install.sh
@@ -1,20 +1,64 @@
 #!/bin/sh
 
 HDD="/dev/sda"
-ENCRYPT=full
-USAGE="server"
-PASSWD=$1
-HOSTNAME=nixos-test
+USER=""
+PASSWORD=""
+HOSTNAME=""
+ENCRYPT=""
+KEY=""
+TYPE=""
 
+# Include the common helper functions.
+. $(dirname "$0")/helpers.sh
+
+################################################################################
+# Check whether the combination of arguments look sensible.
+################################################################################
+validate_args() {
+  if [[ ${ENCRYPT} == "full" || ${ENCRYPT} == "root" ]]; then
+    if [ ${KEY} == "" ]; then
+      ehco "An ecryption key must be specified using -k or --key."
+    fi
+  fi
+}
+
+################################################################################
+# EXEC START
+################################################################################
+# Parse the arguments to the script.
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    -u) USER="$2"; shift 2;;
+    -p) PASSWORD="$2"; shift 2;;
+    -h) HOSTNAME="$2"; shift 2;;
+    -e) ENCRYPT="$2"; shift 2;;
+    -k) KEY="$2"; shift 2;;
+    -t) TYPE="$2"; shift 2;;
+
+    --user=*) USER="${1#*=}"; shift 1;;
+    --password=*) PASSWORD="${1#*=}"; shift 1;;
+    --hostname=*) HOSTNAME="${1#*=}"; shift 1;;
+    --encrypt=*) ENCRPT="${1#*=}"; shift 1;;
+    --key=*) KEY="${1#*=}"; shift 1;;
+    --type=*) TYPE="${1#*=}"; shift 1;;
+
+    *) echo "invalid argument: $1" >&2; exit 1;;
+  esac
+done
+
+must_be_root ${LINENO}
+
+# Check that the script args are valid.
+validate_args
 
 # Configure the hard-drive.
 case ${ENCRYPT} in
   "full")
-    $(dirname "$0")/hdd_setup/encrypt_full.sh ${HDD} ${PASSWD}
+    $(dirname "$0")/hdd_setup/encrypt_full.sh ${HDD} ${PASSWORD}
     ;;
   
   "root")
-    $(dirname "$0")/hdd_setup/encrypt_full.sh ${HDD} ${PASSWD}
+    $(dirname "$0")/hdd_setup/encrypt_full.sh ${HDD} ${PASSWORD}
     ;;
 
   *)
@@ -28,6 +72,25 @@ nixos-generate-config --root /mnt --no-filesystems
 
 # Copy the configuration of the system.
 cp -f $(dirname "$0")/conf/configuration.nix "/mnt/etc/nixos/"
+check_error ${LINENO} "Failed to copy default configuration file."
+
+# Generate the user configuration.
+echo "{ config, pkgs, ... }:
+{
+  users.users = {
+    # Disable root logins.
+    root = {
+      hashedPassword = \"!\";
+    };
+
+    # Create a the default user with sudo access.
+    ${USER} = {
+      isNormalUser = true;
+      extraGroups = [ \"wheel\" ];
+      hashedPassword = \"$(mkpasswd -m sha-512)\";
+    };
+  };
+}" >> /mnt/etc/nixos/conf/users.nix
 
 # Rebuild the distribution.
 nixos-install
